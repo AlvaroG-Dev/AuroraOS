f601c24bd3e55bbd620d9b7fa8dde7224d47d4a6
Alvaro G. <jk123yl2@gmail.com>
2026-09-20 21:58:13 +0200

    Load pftest application after running tests

--- kernel/main.c
+++ kernel/main.c
@@ -255,6 +255,7 @@ static void kmain_task(void) {
   if (failed > 0) {
     LOG_ERR("[TEST] %d tests fallaron. Revisar arriba.", failed);
   }
+  process_load("apps/pftest");
 
   while (1) {
     sched_yield();
