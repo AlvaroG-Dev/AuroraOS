64a277e1bd8f8c265f8dca23e06f23bbb95571a7
Alvaro G. <jk123yl2@gmail.com>
2026-09-20 22:20:41 +0200

    Modify TARFS load message in check_boot.sh
    Updated TARFS load message to remove node count.

--- scripts/check_boot.sh
+++ scripts/check_boot.sh
@@ -292,7 +292,7 @@ EXPECTED_BOOT=(
     "[PF] Demand paging inicializado"
     "[HEAP] Heap inicializado"
     "[SLAB] Inicializado: 8 caches"
-    "[TARFS] Carga completa. 21 nodos registrados."
+    "[TARFS] Carga completa. nodos registrados."
     "[RTC] CMOS RTC detectado"
     "[SCHED] Scheduler + SSE/FPU SMP inicializado"
     "[IPC] Subsistema de Paso de Mensajes inicializado."
