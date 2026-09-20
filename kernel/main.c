282164b180fa7f4ce0d762b7ee93e5c8ff450c3a
Alvaro G. <jk123yl2@gmail.com>
2026-09-20 22:08:26 +0200

    Change process load from pftest to hello

--- kernel/main.c
+++ kernel/main.c
@@ -255,7 +255,7 @@ static void kmain_task(void) {
   if (failed > 0) {
     LOG_ERR("[TEST] %d tests fallaron. Revisar arriba.", failed);
   }
-  process_load("apps/pftest");
+  process_load("apps/hello");
 
   while (1) {
     sched_yield();
