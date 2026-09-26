fadc73cdf8b674cc8b6d0f34112b31635cfe0fe6
Alvaro G. <jk123yl2@gmail.com>
2026-09-26 16:50:39 +0200

    smp: prevent stale timeout wakeups after wait requeue

--- kernel/wait.h
+++ kernel/wait.h
@@ -47,3 +47,5 @@ void wake_up_interruptible_all(wait_queue_t *wq);
 void wake_up_one_locked(wait_queue_t *wq);
 
 #endif
+void wait_queue_wake_timeout_task(wait_queue_t *wq, task_t *task,
+                                  uint64_t wait_seq);
